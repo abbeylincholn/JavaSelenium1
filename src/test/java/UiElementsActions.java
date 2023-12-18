46330b5d1ee7c5a27bdbc987cf69758f30f60324
Abiodun Sholanke <abbeylincholn4real@gmail.com>
2023-12-18 18:21:57 +0000

    The select element (Drop Down)

--- src/test/java/UiElementsActions.java
+++ src/test/java/UiElementsActions.java
@@ -3,17 +3,20 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+import java.io.File;
 
 public class UiElementsActions {
     public static void main(String[] args) throws InterruptedException {
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
 
-        driver.get("http://omayo.blogspot.com/");
+        driver.get("https://omayo.blogspot.com/search?q=test");
         driver.manage().window().maximize();
         Thread.sleep(3000);
 
-        // Display Radio Buttons Selection
+     // Display Radio Buttons Selection Option  -----------------------------------------------------------
  /*     WebElement radioOptionMale = driver.findElement(By.id("radio1"));
         WebElement radioOptionFemale = driver.findElement(By.id("radio2"));
         UiElementsActions uiElementsActions = new UiElementsActions();
@@ -45,7 +48,44 @@ public class UiElementsActions {
         }else {
             System.out.println(" Female is not Selected ");
         }
-        */ // Display Radio Buttons Selection
+        */ // Display Radio Buttons Selection Option
+ //-------------------------------------------------------------------------------------------
+
+ // The select element (Drop Down) -----------------------------------------------------------------------
+   /*    WebElement oldNewsLetters = driver.findElement(By.id("drop1"));
+        Select oldNewsLettersSelect  = new Select(oldNewsLetters);
+        oldNewsLettersSelect.selectByValue("mno");
+        Thread.sleep(2000);
+        oldNewsLettersSelect.selectByVisibleText("doc 2");
+        Thread.sleep(2000);
+        oldNewsLettersSelect.selectByIndex(1);
+        Thread.sleep(2000);
+
+        System.out.println(" Select Text " + oldNewsLetters.getText());
+        //System.out.println("Select Text" + oldNewsLettersSelect.getFirstSelectedOption().getText()); */
+ // The select element (Drop Down) -----------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     }
 }
